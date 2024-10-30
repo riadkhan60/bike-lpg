1b93f55d22198e2d689c01e4751beb5cacd496e2
riadkhan60 <khanriad60@gmail.com>
2024-10-30 15:03:32 +0600

    images cms added v8

--- src/components/dashBoard/dasboard-cms/Dashboard.tsx
+++ src/components/dashBoard/dasboard-cms/Dashboard.tsx
@@ -28,7 +28,7 @@ export default function Dashboard() {
         <Button className="mt-5">
           <Link href={'/'}>Go Home</Link>
         </Button>
-        <div>devsd</div>
+        <div>devsadadasd</div>
       </CardContent>
     </Card>
   );
