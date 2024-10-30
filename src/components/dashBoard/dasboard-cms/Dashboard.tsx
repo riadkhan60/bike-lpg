90614c1a9a353f4dc6a0b27b4272b6239f1e98b2
riadkhan60 <khanriad60@gmail.com>
2024-10-30 15:37:49 +0600

    workflow testing v7

--- src/components/dashBoard/dasboard-cms/Dashboard.tsx
+++ src/components/dashBoard/dasboard-cms/Dashboard.tsx
@@ -28,7 +28,7 @@ export default function Dashboard() {
         <Button className="mt-5">
           <Link href={'/'}>Go Home</Link>
         </Button>
-        <div>devsad</div>
+        <div>devsadafafaf</div>
       </CardContent>
     </Card>
   );
